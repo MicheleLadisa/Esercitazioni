00460245b4c576ee74e397c7d303e999eb0692da
MicheleLadisa <ladisamichele@gmail.com>
2018-03-04 21:21:20 +0100

    flatdice

--- FlatDice/app/src/main/java/com/example/ladis/flatdice/FragmentDice.java
+++ FlatDice/app/src/main/java/com/example/ladis/flatdice/FragmentDice.java
@@ -1,9 +1,45 @@
 package com.example.ladis.flatdice;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.Random;
+
 /**
  * Created by ladis on 04/03/2018.
  */
 
-public class FragmentDice {
-
+public class FragmentDice  extends Fragment{
+     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+         Random r=new Random();
+         View v=inflater.inflate(R.layout.dicefragment, container, false);
+         ImageView iv=(ImageView) v.findViewById(R.id.DiceImageView);
+         int diceface=1+r.nextInt(6);
+         switch (diceface) {
+             case 1:
+                 iv.setImageResource(R.drawable.dice1);
+                 break;
+             case 2:
+                 iv.setImageResource(R.drawable.dice1);
+                 break;
+             case 3:
+                 iv.setImageResource(R.drawable.dice1);
+                 break;
+             case 4:
+                 iv.setImageResource(R.drawable.dice1);
+                 break;
+             case 5:
+                 iv.setImageResource(R.drawable.dice1);
+                 break;
+             case 6:
+                 iv.setImageResource(R.drawable.dice1);
+                 break;
+         }
+         return v;
+     }
 }
--- FlatDice/app/src/main/java/com/example/ladis/flatdice/MainActivity.java
+++ FlatDice/app/src/main/java/com/example/ladis/flatdice/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ladis.flatdice;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +17,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(findViewById(R.id.fragment)!=null)
+        {
+            FragmentDice fd= new FragmentDice();
+            FragmentTransaction ft=getFragmentManager().beginTransaction();
+            ft.replace(R.id.fragment,fd);
+            ft.commit();
+        }
+    }
+
+    public boolean onTouchEvent(MotionEvent event){
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1=event.getX();
+                y1=event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2=event.getX();
+                y2=event.getY();
+
+                float deltax=x2-x1;
+                float deltay=y2-y1;
+
+                if(Math.abs(deltax)>MIN_DISTANCE)
+                {
+                    if(x2>x1){
+                        changeFragment("left");
+                    }
+                    else{
+                        changeFragment("rigth");
+                    }
+                }
+                else if(Math.abs(deltay)>MIN_DISTANCE)
+                {
+                    if(y2>y1){
+                        changeFragment("top");
+                    }
+                    else{
+                        changeFragment("bot");
+                    }
+                }
+                break;
+        }
+        return true;
+    }
+
+    public void changeFragment(String direction){
+
     }
 }
